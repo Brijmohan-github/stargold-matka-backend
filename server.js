7cddb8d6c5273db890d92c36c8903a4b3640bc1a
bmkaradia <bmk302006@gmail.com>
2024-12-26 11:33:40 +0530

    update cors

--- server.js
+++ server.js
@@ -6,6 +6,17 @@ connect(process.env.DB_URI)
   .then(() => console.log("DB connection successfull!...."))
   .catch((err) => console.log(err.message, `\n Db Connection Failed!...`));
 
+
+  const corsOptions = {
+    origin: ['https://admin.ratankhichi.com'], // Allow multiple origins
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Allow cookies or authentication headers
+    optionsSuccessStatus: 204, // For legacy browsers
+  };
+  
+  app.use(cors(corsOptions));
+  
 // starting the server
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
